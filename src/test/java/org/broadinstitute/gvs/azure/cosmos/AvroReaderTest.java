689086cf40fe5570e089ce79d33e66c26e8883a8
Miguel Covarrubias <mcovarr@broadinstitute.org>
2023-04-21 20:47:09 -0400

    cleanup

--- src/test/java/org/broadinstitute/gvs/azure/cosmos/AvroReaderTest.java
+++ src/test/java/org/broadinstitute/gvs/azure/cosmos/AvroReaderTest.java
@@ -19,35 +19,6 @@ public class AvroReaderTest {
     }
 
     @Test
-    public void testObjectNodeFromString() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String stringJson = """
-                {
-                    "sample_id": 7,
-                    "location": 1000000010327,
-                    "ref": "T",
-                    "alt": "C",
-                    "AS_RAW_MQ": "0|4842",
-                    "AS_RAW_MQRankSum": null,
-                    "QUALapprox": "72",
-                    "AS_QUALapprox": "72",
-                    "AS_RAW_ReadPosRankSum": null,
-                    "AS_SB_TABLE": "0,0|0,3",
-                    "AS_VarDP": "0|3",
-                    "call_GT": "1/1",
-                    "call_AD": "0,3",
-                    "call_GQ": 7,
-                    "call_PGT": "0|1",
-                    "call_PID": "10327_T_C",
-                    "call_PL": "72,7,0,72,7,72"
-                }
-                """.trim();
-        ObjectNode objectNode = AvroReader.objectNodeFromAvroRecord(objectMapper, stringJson);
-
-        Assert.assertEquals(objectNode.get("sample_id").asLong(), 7L);
-    }
-
-    @Test
     public void testObjectNodesForAvroPath() {
         ObjectMapper objectMapper = new ObjectMapper();
         List<Path> avroFiles = AvroReader.findAvroPaths("src/test/resources/vets");
